69b5f940f34660039396a4a611f04c931117030a
Marcelo Silva <msilva@arkhotech.com>
2017-08-29 19:48:50 +0000

    Refactorización

--- simple-swarm/restart-fpm.sh
+++ simple-swarm/restart-fpm.sh
@@ -2,4 +2,10 @@
 
 CONTAINER_ID=`docker ps -f NAME=simple_simple -q`
 
-docker exec -ti ${CONTAINER_ID} /etc/init.d/php5-fpm restart
+
+for nodo in $(docker service ps --format "{{.Name}}" -f NAME=simple_simple -f desired-state=running simple) 
+do
+echo $nodo
+docker exec -ti ${nodo} /etc/init.d/php5-fpm restart
+
+done
--- simple/restart-fpm.sh
+++ simple/restart-fpm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CONTAINER_ID=`docker ps -f NAME=simple_simple_ -q`
+CONTAINER_ID=`docker ps -f NAME=simple_simple -q`
 
 docker exec -ti ${CONTAINER_ID} /etc/init.d/php5-fpm restart
