ee28f5bef81f52b2592f1b336d14d5537a6d283a
Marcelo Silva <msilva@arkhotech.com>
2017-09-06 15:54:40 +0000

    Modificación imagenes para SWARM

--- simple-swarm/restart-fpm.sh
+++ simple-swarm/restart-fpm.sh
@@ -3,7 +3,7 @@
 CONTAINER_ID=`docker ps -f NAME=simple_simple -q`
 
 
-for nodo in $(docker service ps --format "{{.Name}}" -f NAME=simple_simple -f desired-state=running simple) 
+for nodo in $(docker ps -f NAME=simple_simple -q) 
 do
 echo $nodo
 docker exec -ti ${nodo} /etc/init.d/php5-fpm restart
